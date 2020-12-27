fb7a2b05c5462f76786521db35ca8695d142f6fc
Roman Perevalov <roman1490@yandex.ru>
2020-12-27 21:39:49 +0700

    done with JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
 public class Main {
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+
+        userDao.saveUser("Иван", "Иванов", (byte) 20);
+        userDao.saveUser("Петр", "Петров", (byte) 20);
+        userDao.saveUser("Федор", "Федоров", (byte) 20);
+        userDao.saveUser("Николай", "Николаев", (byte) 20);
+
+        System.out.println(userDao.getAllUsers());
+
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
